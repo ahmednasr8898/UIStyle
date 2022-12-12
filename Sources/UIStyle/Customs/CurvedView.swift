24e69d228818945b34b5438017376f1e3e0fe93a
Ahmed Nasr <ahmed.nasr.saieed@gmail.com>
2022-12-12 15:36:31 +0200

    edit in curved view

--- Sources/UIStyle/Customs/CurvedView.swift
+++ Sources/UIStyle/Customs/CurvedView.swift
@@ -13,10 +13,10 @@ public class CurvedView: UIView {
     
     public override func draw(_ rect: CGRect) {
         let color = UIColor.white
-        let y:CGFloat = -194
+        let y:CGFloat = -self.frame.height * 0.999 //-194
         let myBezier = UIBezierPath()
         myBezier.move(to: CGPoint(x: 0, y: y))
-        myBezier.addQuadCurve(to: CGPoint(x: rect.width, y: y), controlPoint: CGPoint(x: rect.width / 2, y: rect.height + 78))
+        myBezier.addQuadCurve(to: CGPoint(x: rect.width, y: y), controlPoint: CGPoint(x: rect.width / 2, y: rect.height + /*78*/ self.frame.height * 0.33))
         myBezier.addLine(to: CGPoint(x: rect.width, y: rect.height))
         myBezier.addLine(to: CGPoint(x: 0, y: rect.height))
         myBezier.close()
